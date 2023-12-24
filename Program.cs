50af5bd4c3ca0e12094130a21b77c33c07d5de45
GunaVarma11 <gunavarmagv@gmail.com>
2023-12-24 12:58:31 +0530

    inspect

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+
+
+        // Specify the path to your text file
+        string filePath = @"R:\Guna\Student.txt ";
+
+        if (File.Exists(filePath))
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        else
+        {
+            Console.WriteLine("invalid file path");
+
+        }
+
+    }
+}
